0f6cd3b7b9c22944e8db7d6cf092264191d5dbb4
Ijon Tichy <gztichy@lavabit.com>
2013-03-27 13:22:21 -0500

    Plasma climbing has been nerfed, air control is forced to 0.15 unless cyberrunner_noaircontrol is 1, dashing is no longer ludicrously OP

--- pk3/acs/cyberrun.c
+++ pk3/acs/cyberrun.c
@@ -35,29 +35,42 @@ function int magnitudeThree_f(int x, int y, int z)
     return len;
 }
 
-script 400 ENTER
+script 405 OPEN
 {
+    if (!GetCVar("cyberrunner_noaircontrol"))
+    {
+        ConsoleCommand("set cyberrunner_noaircontrol 0");
+        ConsoleCommand("archivecvar cyberrunner_noaircontrol");
+    }
+
     while (1)
     {
-        if (Timer() % 5 == 0)
+        if (GetCVar("sv_aircontrol") != 0.15 && !GetCVar("cyberrunner_noaircontrol"))
+        {
+            ConsoleCommand("sv_aircontrol 0.15");
+        }
+        Delay(1);
+    }
+}
+
+script 400 ENTER
+{
+    int time = 0;
+
+    while (1)
+    {
+        if (time % 5 == 0)
         {
             TakeInventory("DashCooldown",1);
             TakeInventory("JumpCooldown",1);
             TakeInventory("BoostCooldown",1);
         }
-        if (Timer() % 20 == 0) { GiveInventory("PlasmaGunAmmo",1); }
+        if (time % 20 == 0) { GiveInventory("PlasmaGunAmmo",1); }
+        if (time % 105 == 0) { GiveInventory("ForceVentAmmo", 1); }
 
+        time++;
         delay(1);
     }
-}
-
-script 405 ENTER
-{
-	while (TRUE)
-	{
-        GiveInventory("ForceVentAmmo",1);
-        delay(105);
-	}
 }
 
 script 401 DEATH
@@ -68,9 +81,13 @@ script 401 DEATH
     ACS_Terminate(400, 0);
 }
 
+#define DASH_VEL 45
+
 script 402 (void) NET
 {
-    int vx, vy, vz, mag, angle, pitch;
+    int vx, vy, vz,  mag, angle, pitch;
+    int nx, ny, nz, nmag;
+    int rx, ry, rz, rmag;
 
     if (CheckInventory("DashCooldown") == 0)
     {
@@ -78,15 +95,32 @@ script 402 (void) NET
         vy = GetActorVelY(0);
         vz = GetActorVelZ(0);
         angle = GetActorAngle(0);
-        pitch = middle(-0.05, GetActorPitch(0), 0.05);
+        pitch = middle(-0.027, GetActorPitch(0), 0.05);
         mag = magnitudeThree_f(vx, vy, vz);
-        mag += 42.0;
+
+        nx = FixedMul(DASH_VEL * cos(angle), cos(pitch));
+        ny = FixedMul(DASH_VEL * sin(angle), cos(pitch));
+        nz = DASH_VEL * -sin(pitch);
+        nmag = magnitudeThree_f(nx, ny, nz);
+        
+        rx = vx + nx; ry = vy + ny; rz = vz + nz;
+        rmag = magnitudeThree_f(rx, ry, rz);
 
         ActivatorSound("cyber/dash",255);
-        SetActorVelocity(0,
-                FixedMul(FixedMul(mag, cos(angle)), cos(pitch)),
-                FixedMul(FixedMul(mag, sin(angle)), cos(pitch)),
-                FixedMul(mag, -sin(pitch)), 0, 0);
+
+        if (rmag < mag)  // if we're making a sharp turn
+        {
+            SetActorVelocity(0, nx, ny, nz, 0,0);
+        }
+        else
+        {
+            mag += (DASH_VEL<<16);
+
+            SetActorVelocity(0,
+                    FixedMul(FixedMul(mag, cos(angle)), cos(pitch)),
+                    FixedMul(FixedMul(mag, sin(angle)), cos(pitch)),
+                    FixedMul(mag, -sin(pitch)), 0, 0);
+        }
 
         GiveInventory("DashCooldown",30);
     }
